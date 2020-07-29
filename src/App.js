a29d72ca84ffe31e1ae9722489467c377378735d
GabrielaMachado <gabimachadop@hotmail.com>
2020-07-29 15:08:06 -0500

    commit issues

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-<<<<<<< HEAD
 import Login from "./components/login/Login";
 import Register from "./components/login/Register";
 import BackLogin from "./components/login/backgroun.jpg";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Issues from "./components/issues/issues";
 
 function App() {
   return (
@@ -14,17 +14,12 @@ function App() {
           <div className="login">
             <Route exact path="/" component={Login}></Route>
             <Route exact path="/register" component={Register}></Route>
+            <Route exact path="/issues" component={Issues}></Route>
           </div>
         </Switch>
       </div>
     </Router>
   );
-=======
-import IssuesForm from "./components/issues/issuesForm";
-
-function App() {
-  return <React.Fragment><IssuesForm></IssuesForm></React.Fragment>
->>>>>>> 5c1e5de... first commit issues
 }
 
 export default App;
